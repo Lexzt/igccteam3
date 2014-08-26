d072bb9092589278552d518be05f9e4eb7e4f5cf
meguro.1221@gmail.com <meguro.1221@gmail.com@28eeb29f-55b9-8db0-c07d-95f2ba84ce78>
2014-08-26 08:43:27 +0000

    can rotate to Swipe and stop to tap; but few Bugs in Swipe; i'll fix;

--- Project/Assets/_Assets/_Scripts/Pointer.cs
+++ Project/Assets/_Assets/_Scripts/Pointer.cs
@@ -15,38 +15,38 @@ public class Pointer : MonoBehaviour {
         RaycastHit hit;
         if (Physics.Raycast(PointerPosition.position, Vector3.forward, out hit, Mathf.Infinity))
         {
-            if (hit.transform.renderer.material.color == Color.red)
-            {
-                Debug.Log("Red");
-            }
-            else if (hit.transform.renderer.material.color == Color.blue)
-            {
-                Debug.Log("Blue");
-            }
-            else if (hit.transform.renderer.material.color == Color.black)
-            {
-                Debug.Log("Black");
-            }
-            else if (hit.transform.renderer.material.color == Color.cyan)
-            {
-                Debug.Log("Cyan");
-            }
-            else if (hit.transform.renderer.material.color == Color.gray)
-            {
-                Debug.Log("Gray");
-            }
-            else if (hit.transform.renderer.material.color == Color.green)
-            {
-                Debug.Log("Green");
-            }
-            else if (hit.transform.renderer.material.color == Color.magenta)
-            {
-                Debug.Log("Magenta");
-            }
-            else if (hit.transform.renderer.material.color == Color.yellow)
-            {
-                Debug.Log("Yellow");
-            }
+//            if (hit.transform.renderer.material.color == Color.red)
+//            {
+//                Debug.Log("Red");
+//            }
+//            else if (hit.transform.renderer.material.color == Color.blue)
+//            {
+//                Debug.Log("Blue");
+//            }
+//            else if (hit.transform.renderer.material.color == Color.black)
+//            {
+//                Debug.Log("Black");
+//            }
+//            else if (hit.transform.renderer.material.color == Color.cyan)
+//            {
+//                Debug.Log("Cyan");
+//            }
+//            else if (hit.transform.renderer.material.color == Color.gray)
+//            {
+//                Debug.Log("Gray");
+//            }
+//            else if (hit.transform.renderer.material.color == Color.green)
+//            {
+//                Debug.Log("Green");
+//            }
+//            else if (hit.transform.renderer.material.color == Color.magenta)
+//            {
+//                Debug.Log("Magenta");
+//            }
+//            else if (hit.transform.renderer.material.color == Color.yellow)
+//            {
+//                Debug.Log("Yellow");
+//            }
         }
 	}
 }
---
+++ Project/Assets/_Assets/_Scripts/SwipeManager.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwipeManager : MonoBehaviour
+{
+
+	/// 変数定義
+	public float rotSpeed = 0.01f;
+	public Vector2 inputPosition;
+	public Vector2 inputPositionBuffer;
+	public float flickDistance = 0.0f;
+
+	private PieController pCtrler;
+	private bool isClick = false;
+	private Vector2 objectPos;
+	void Start()
+	{
+		isClick = false;
+		pCtrler = GetComponent<PieController>();
+	}
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButtonDown(0))
+		{
+			onClickObject();
+		}
+		
+		if(Input.GetMouseButtonUp(0)){
+			onClickEnd();
+		}
+	}
+	
+	private void onClickObject(){
+		GameObject selectGameObject;// クリックされたゲームオブジェクトへの参照.
+
+		// レイを取得.
+		Ray clkRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+		RaycastHit rh;
+		//当たり判定
+		if (Physics.Raycast(clkRay, out rh, 500)){
+			selectGameObject = rh.collider.gameObject;// クリックされたゲームオブジェクトを格納.
+			print (selectGameObject);
+			if(selectGameObject.name == "Cylinder_1_Part(Clone)")
+			{
+				isClick = true;
+			}
+			//クリックした際のポジションを取得
+			inputPosition = selectGameObject.transform.position;
+			inputPositionBuffer = Input.mousePosition;
+			objectPos = Camera.main.WorldToScreenPoint(selectGameObject.transform.position);
+		}
+		else{
+			selectGameObject = null;// 何もクリックされていない場合はnullを代入
+		}
+	}
+	/**
+	 * @クリックされたときの処理
+	*/
+	private void onClickEnd () {
+
+		if (!isClick) return;
+
+		// ドラッグ距離を判定
+		//クリックした際のポジションを取得
+		inputPosition = inputPositionBuffer;
+		inputPositionBuffer = Input.mousePosition;
+		// 2つのベクトルの角度を比較して右回転か左回転かを判断する
+		float angle1 = Mathf.Atan2(inputPositionBuffer.x-objectPos.x,inputPositionBuffer.y-objectPos.y);
+		float angle2 = Mathf.Atan2(inputPosition.x-objectPos.x,inputPosition.y-objectPos.y);
+		float subAngle = angle1 - angle2;// プラスなら右回転
+		float rotScale = 0;
+		rotScale = Vector2.Angle(inputPositionBuffer-objectPos, inputPosition-objectPos);
+		if(subAngle < 0){
+			rotScale *= -1.0f;
+		}
+		// その分だけ回転速度をアップする
+		print (rotScale);
+		if(Mathf.Abs(rotScale) < 0.03f)
+		{
+			pCtrler.StopRotate();
+		}else{
+			rotScale *= 0.01f;
+			pCtrler.IncreaseSpeed (rotScale);
+		}
+		isClick = false;
+	}
+}
