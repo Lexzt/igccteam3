61b9d754ee65c6b2045bb41f6dbb274c6cf9c148
meguro.1221@gmail.com <meguro.1221@gmail.com@28eeb29f-55b9-8db0-c07d-95f2ba84ce78>
2014-08-28 02:17:20 +0000

    change Swipe Mechanique

--- Project/Assets/_Assets/_Scripts/SwipeManager.cs
+++ Project/Assets/_Assets/_Scripts/SwipeManager.cs
@@ -6,14 +6,14 @@ public class SwipeManager : MonoBehaviour
 
 	/// 変数定義
 
-	public float 		accelSpeedValue = 10.0f;
-	public float		tapRangeDetection = 0.03f;
-	private Vector2 	inputPosition;
-	private Vector2 	inputPositionBuffer;
-	private PieController pCtrler;
-	private bool 		isClick = false;
-	private Vector2 	objectPos;
-	private GameObject 	selectGameObject;// クリックされたゲームオブジェクトへの参照.
+	public float 			accelSpeedValue = 10.0f;
+	public float			tapRangeDetection = 0.03f;
+	/// private
+	private Vector2 		inputPositionStart;
+	private PieController 	pCtrler;
+	private bool 			isClick = false;
+	private Vector2 		objectPos;
+	private GameObject 		selectGameObject;// クリックされたゲームオブジェクトへの参照.
 	void Start()
 	{
 		isClick = false;
@@ -21,44 +21,29 @@ public class SwipeManager : MonoBehaviour
 	}
 	// Update is called once per frame
 	void Update () {
-//=========================
-//TODO: DELETE AFTER ATTACH COLLIDER
 		if(Input.GetMouseButtonDown(0))
 		{
 			onClickObject();
 		}
 		if(Input.GetMouseButtonUp(0)){
-			onClickEnd();
+			onReleased();
 		}
-//=========================
 	}
-
-	void OnMouseDown()
-	{
-		onClickObject();
-	}
-	void OnMouseUp()
-	{
-		onClickEnd();
-	}
-
 
 	private void onClickObject(){
 
+		inputPositionStart = Input.mousePosition;
 		// レイを取得.
 		Ray clkRay = Camera.main.ScreenPointToRay(Input.mousePosition);
 		RaycastHit rh;
 		//当たり判定
 		if (Physics.Raycast(clkRay, out rh, 500)){
 			selectGameObject = rh.collider.gameObject;// クリックされたゲームオブジェクトを格納.
-			if(selectGameObject.name == "Cylinder_1_Part(Clone)")
-			{
-				isClick = true;
-			}
-			//クリックした際のポジションを取得
-			inputPosition = selectGameObject.transform.position;
-			inputPositionBuffer = Input.mousePosition;
 			objectPos = Camera.main.WorldToScreenPoint(pCtrler.gameObject.transform.position);
+//			if(selectGameObject.name == "Cylinder_1_Part(Clone)")
+//			{
+//				isClick = true;
+//			}
 		}
 		else{
 			selectGameObject = null;// 何もクリックされていない場合はnullを代入
@@ -67,39 +52,25 @@ public class SwipeManager : MonoBehaviour
 	/**
 	 * @クリックされたときの処理
 	*/
-	private void onClickEnd () {
+	private void onReleased () {
 
-		if (!isClick) return;
+		//if (!isClick) return;
 
-		// ドラッグ距離を判定
-		//クリックした際のポジションを取得
-		inputPosition = inputPositionBuffer;
-		inputPositionBuffer = Input.mousePosition;
-		// 2つのベクトルの角度を比較して右回転か左回転かを判断する
-		float angle1 = Mathf.Atan2(inputPositionBuffer.x-objectPos.x, inputPositionBuffer.y	-objectPos.y);
-		float angle2 = Mathf.Atan2(inputPosition.x		-objectPos.x, inputPosition.y		-objectPos.y);
-		float subAngle = angle1 - angle2;// プラスなら右回転
-		float rotScale = 0;	// value of Swipe
-		rotScale = Vector2.Angle(inputPositionBuffer-objectPos, inputPosition-objectPos);
-		if(subAngle < 0){
-			rotScale *= -1.0f;
-		}
+		Vector2 releasePosition = Input.mousePosition;
 		//Calc Swipe
-		Vector3 subVec =  inputPositionBuffer - inputPosition;
+		Vector2 subVec =  releasePosition - inputPositionStart;
 
-		if(Mathf.Abs(rotScale) < tapRangeDetection)
+		if(Mathf.Abs(subVec.y) < tapRangeDetection)
 		{
 			pCtrler.StopRotate();
 		}else{// if Swipe
-			print (inputPosition);
-			if(Screen.width/2 > inputPosition.x){
+			if(Screen.width/2 > inputPositionStart.x){
 				// Swipe Up
 				if( subVec.y > 0)	pCtrler.IncreaseSpeed (accelSpeedValue);
 			}else{
 				// Swipe Down
-				if( subVec.y > 0)	pCtrler.IncreaseSpeed (-accelSpeedValue);
+				if( subVec.y < 0)	pCtrler.IncreaseSpeed (accelSpeedValue);
 			}
 		}
-		isClick = false;
 	}
 }
